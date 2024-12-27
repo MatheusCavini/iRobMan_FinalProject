a9d901a2f8408314805ac9b9f5044a7448f413fc
Sohan <sohan.rudra@tu-darmstadt.de>
2024-12-27 12:59:54 +0000

    Update utils.py fixing height and width interchange.

--- src/utils.py
+++ src/utils.py
@@ -14,15 +14,15 @@ def pb_image_to_numpy(rgbpx, depthpx, segpx, width, height):
 
     Returns:
         Tuple of:
-            rgb: RGBA image as numpy array [width, height, 4]
-            depth: Depth map as numpy array [width, height]
-            seg: Segmentation map as numpy array [width, height]
+            rgb: RGBA image as numpy array [height, width, 4]
+            depth: Depth map as numpy array [height, width]
+            seg: Segmentation map as numpy array [height, width]
     """
     # RGBA - channel Range [0-255]
-    rgb = np.reshape(rgbpx, [width, height, 4])
+    rgb = np.reshape(rgbpx, [height, width, 4])
     # Depth Map Range [0.0-1.0]
-    depth = np.reshape(depthpx, [width, height])
+    depth = np.reshape(depthpx, [height, width])
     # Segmentation Map Range {obj_ids}
-    seg = np.reshape(segpx, [width, height])
+    seg = np.reshape(segpx, [height, width])
 
     return rgb, depth, seg
